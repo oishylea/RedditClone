0ff67d7ce4a8eac2d483992db0dc64c25c9c97b3
Arini <arinifatihah710@gmail.com>
2024-10-18 01:34:39 +0800

    Delete Community & Pagination

--- reddit-clone/resources/js/Pages/Communities/Index.vue
+++ reddit-clone/resources/js/Pages/Communities/Index.vue
@@ -16,24 +16,11 @@
               <Link
                 :href="route('communities.create')"
                 class="
-                  inline-flex
-                  items-center
-                  justify-center
-                  rounded-md
-                  border border-transparent
-                  bg-indigo-600
-                  px-4
-                  py-2
-                  text-sm
-                  font-medium
-                  text-white
-                  shadow-sm
-                  hover:bg-indigo-700
-                  focus:outline-none
-                  focus:ring-2
-                  focus:ring-indigo-500
-                  focus:ring-offset-2
-                  sm:w-auto
+                  inline-flex items-center justify-center
+                  rounded-md border border-transparent
+                  bg-indigo-600 px-4 py-2 text-sm font-medium text-white
+                  shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2
+                  focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto
                 "
               >
                 Add Community
@@ -50,13 +37,13 @@
                       <tr>
                         <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
                         <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Slug</th>
-                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 text-gray-900">
                           <span class="sr-only">Edit</span>
                         </th>
                       </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200 bg-white">
-                      <tr v-for="community in communities" :key="community.id">
+                      <tr v-for="community in communities.data" :key="community.id">
                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                           {{ community.name }}
                         </td>
@@ -64,14 +51,18 @@
                           {{ community.slug }}
                         </td>
                         <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          <Link :href="route('communities.edit', community.id)" class="text-indigo-600 hover:text-indigo-900">Edit</Link>
-                          <!-- <Link :href="route('communities.destroy', community.slug)" method="delete" class="text-red-600 hover:text-red-900">Delete</Link> -->
+                          <Link :href="route('communities.edit', community.id)" class="text-indigo-600 hover:text-indigo-900 mr-3">
+                            Edit
+                          </Link>
+                          <Link :href="route('communities.destroy', community.id)" class="text-red-600 hover:text-red-900" 
+                          method="delete" as="button" type="button">Delete</Link>
                         </td>
                       </tr>
                     </tbody>
                   </table>
+
                   <div class="m-2 p-2">
-                    <Pagination :links="communities.links" />
+                    <Pagination :links="communities.links"/>
                   </div>
                 </div>
               </div>
@@ -82,14 +73,31 @@
     </div>
   </AuthenticatedLayout>
 
-  <pre>{{ communities }}</pre>
+  <!--<pre>{{ communities }}</pre>-->
 </template>
 
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { Head, Link } from "@inertiajs/inertia-vue3";
+import { Head, Link,} from "@inertiajs/inertia-vue3";
+import Pagination from "../../Components/Pagination.vue";
 
 const props = defineProps({
-  communities: Array // Update to expect an Array
+  communities: Object, // Expecting a paginated object (not an array)
 });
+
+// Method to handle delete
+/*function confirmCommunity(id) {
+  if (confirm("Are you sure you want to delete this community?")) {
+    router.delete(route('communities.destroy', id), {
+      onSuccess: () => {
+        alert('Community deleted successfully');
+        router.reload();
+      },
+      onError: (errors) => {
+        console.error(errors);
+        alert('Failed to delete the community.');
+      }
+    });
+  }
+}*/
 </script>

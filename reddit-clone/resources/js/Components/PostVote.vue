900705383bcd8c1d296414cdd417705060e72d07
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-11-01 11:52:15 +0800

    no more down vote

--- reddit-clone/resources/js/Components/PostVote.vue
+++ reddit-clone/resources/js/Components/PostVote.vue
@@ -3,9 +3,19 @@
     <div class="w-12 flex flex-col m-2 p-2">
         <Link :href="route('posts.upVote', post)" 
             method="post" as="button" type="button"
-            class="text-grey-400 hover:text-blue-500"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            class="text-grey-400 hover:text-blue-500">
+
+            <!-- up -->
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+            </svg> -->
+
+            <!-- fire icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
             </svg>
+
         </Link>
         <div>4k</div>
         <!-- <Link :href="route('posts.downVote', post)" 
